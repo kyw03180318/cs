835cccdc72301aed78b7b46373d80478fa7b07af
yewonkang <teardrops700700@gmail.com>
2026-05-27 15:25:32 +0900

    week13 - .gitignore/í4444

--- week 13.py
+++ week 13.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 
+def click_name_button():
+    name=en_name.get()
+    lbl_name.config(text=f"제이름은 {name}입니다")
+
 window = tk.Tk()
-window.title("이름입력")
+window.title("클라우드")
 window.geometry("600x200")
+
 en_name = tk.Entry(window)
-btn_name = tk.button(window, text="클릭")
-lbl_name = tk.Label(window, text ("이름입력:"))
+btn_name = tk.Button(window, text="클릭",command=click_name_button)
+lbl_name = tk.Label(window, text=("이름입력:"))
 
 lbl_name.pack()
 en_name.pack()
