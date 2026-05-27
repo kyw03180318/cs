3c20a15059fb7d8019dc8b3e0ed528588fad2006
yewonkang <teardrops700700@gmail.com>
2026-05-27 15:15:29 +0900

    week13 - .gitignore/파ì333

--- week 13.py
+++ week 13.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("마이 퍼스트 윈도우")
+window.title("이름입력")
 window.geometry("600x200")
+en_name = tk.Entry(window)
+btn_name = tk.button(window, text="클릭")
+lbl_name = tk.Label(window, text ("이름입력:"))
+
+lbl_name.pack()
+en_name.pack()
+btn_name.pack()
 window.mainloop()
